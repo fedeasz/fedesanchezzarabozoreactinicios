6fbc9c84c075d50c79269a24a55b56aac7a1be53
Federico Sanchez Zarabozo <fsanchez@L11fsanchez.central.diazriganti.com>
2022-12-21 18:03:00 -0300

    card y carpetas

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Bienvenidos a nuestro E-comerce";
+  const title2 = "Catalogo";
+  const producto = {
+    precio: 500,
+    nombre: "Miel",
+    Descripcion: "100 % pura",
+  };
+
+  const cardproducto = (
+    <div>
+      <h3>{producto.nombre}</h3>
+      <br></br>
+      <h3>{producto.precio}</h3>
+    </div>
+  );
+  function comprado(){
+    alert("Usted ha adquirido el producto")
+  }
+
+  const miEstilo = {
+    backgroundColor: "orange",
+    margin: "30px",
+    padding:"50px",
+    textAlign: "center",
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {title}
+          <br></br>
+          {title2}
         </p>
         <a
           className="App-link"
@@ -15,9 +43,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
+        {cardproducto}
       </header>
+      <div style={miEstilo} className='catalogo'>{cardproducto}</div>
+      <button onClick={comprado}>COMPRAR</button>
     </div>
   );
 }
